a28e1eaf8eab6c91400f13a9da2ca369825d60f4
jagnyadatta <jagnyadattadalai99215@gmail.com>
2025-07-04 00:11:51 +0530

    Fronted Structure setup done

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import Header from './components/header/Header'
+import DashBoard from './components/header/Navbar'
+
+function App() {
+  return (
+    <>
+      <Header />
+      <DashBoard />
+    </>
+  )
+}
+
+export default App
